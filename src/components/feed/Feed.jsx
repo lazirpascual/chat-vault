4f637f34bf40ce27f688160127a0d1b82089e69a
Lazir Pascual <lazirpascual@gmail.com>
2021-08-09 12:35:14 -0400

    create share component within feed component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Share from "../share/Share";
+import Post from "../post/Post";
 import "./feed.css";
 
 const Feed = () => {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
