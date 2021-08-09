4cad558925c0ef1f6815030f0507dcd1bec2ecc0
Lazir Pascual <lazirpascual@gmail.com>
2021-08-09 13:09:02 -0400

    create post component within feed component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { MoreVert } from "@material-ui/icons";
 import "./post.css";
 
 const Post = () => {
   return (
     <div className="post">
       <div className="postWrapper">
-        <div className="postTop"></div>
-        <div className="postCenter"></div>
-        <div className="postBottom"></div>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUsername">Lazir Pascual</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! it's my first post</span>
+          <img src="assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 People liked this post</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
 import "./share.css";
 
+const shareItems = [
+  {
+    icon: <PermMedia htmlColor="tomato" className="shareIcon" />,
+    name: "Photo or Video",
+  },
+  { icon: <Label htmlColor="blue" className="shareIcon" />, name: "Tag" },
+  {
+    icon: <Room htmlColor="green" className="shareIcon" />,
+    name: "Location",
+  },
+  {
+    icon: <EmojiEmotions htmlColor="goldrenrod" className="shareIcon" />,
+    name: "Reaction",
+  },
+];
+
 const Share = () => {
   return (
     <div className="share">
@@ -17,22 +33,12 @@ const Share = () => {
         <hr className="shareHr" />
         <div className="shareBottom">
           <div className="shareOptions">
-            <div className="shareOption">
-              <PermMedia htmlColor="tomato" className="shareIcon" />
-              <span className="shareOptionText">Photo or Video</span>
-            </div>
-            <div className="shareOption">
-              <Label htmlColor="blue" className="shareIcon" />
-              <span className="shareOptionText">Tag</span>
-            </div>
-            <div className="shareOption">
-              <Room htmlColor="green" className="shareIcon" />
-              <span className="shareOptionText">Location</span>
-            </div>
-            <div className="shareOption">
-              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
-              <span className="shareOptionText">Emotion</span>
-            </div>
+            {shareItems.map((item) => (
+              <div className="shareOption">
+                {item.icon}
+                <span className="shareOptionText">{item.name}</span>
+              </div>
+            ))}
           </div>
           <button className="shareButton">Share</button>
         </div>
