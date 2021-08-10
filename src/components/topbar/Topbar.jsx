e0bd0d108be5358a9fe90b121083b7c49ae2a9a2
Lazir Pascual <lazirpascual@gmail.com>
2021-08-10 18:02:52 -0400

    modify topbar to dynamically fetch user data from api

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Topbar = () => {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -40,7 +44,17 @@ const Topbar = () => {
           </div>
         </div>
       </div>
-      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      <Link to={`/profile/${user.username}`}>
+        <img
+          src={
+            user.profilePicture
+              ? `${PF}${user.profilePicture}`
+              : `${PF}person/noAvatar.png`
+          }
+          alt=""
+          className="topbarImg"
+        />
+      </Link>
     </div>
   );
 };
