ecb1cfa25de2b5484e4e56b127449368c53c8484
Lazir Pascual <lazirpascual@gmail.com>
2021-08-10 14:28:25 -0400

    create email and password refs for input validation in login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./login.css";
 
 const Login = () => {
+  // use useRef instead of useState for forms to prevent rerenders
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.prevent.default();
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,14 +19,27 @@ const Login = () => {
             Connect with friends and the world around you on Chat Vault
           </span>
         </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+        <div className="loginRight" onSubmit={handleSubmit}>
+          <form className="loginBox">
+            <input
+              ref={email}
+              placeholder="Email"
+              type="email"
+              className="loginInput"
+              required
+            />
+            <input
+              ref={password}
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+              minLength="6"
+              required
+            />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">Create New Account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
