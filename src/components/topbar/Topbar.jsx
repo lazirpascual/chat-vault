3e104085197737b488af83476680c439cd85790d
Lazir Pascual <lazirpascual@gmail.com>
2021-08-09 22:47:32 -0400

    add link for home logo in topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Chat Vault</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Chat Vault</span>
+        </Link>
       </div>
       <div className="topbarCenter">
         <div className="searchBar">
