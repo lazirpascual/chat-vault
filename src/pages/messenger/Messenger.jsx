2c025811916dd681c5e83103c638cfdb9120e567
Lazir Pascual <lazirpascual@gmail.com>
2021-08-11 19:08:42 -0400

    fetch user conversations from api

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import ChatOnline from "../../components/chatOnline/ChatOnline";
 import Conversation from "../../components/conversations/Conversation";
 import Message from "../../components/message/Message";
 import Topbar from "../../components/topbar/Topbar";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 import "./messenger.css";
 
 const Messenger = () => {
+  const { user } = useContext(AuthContext);
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get(`/conversations/${user._id}`);
+        setConversations(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+
   return (
     <>
       <Topbar />
@@ -13,11 +30,9 @@ const Messenger = () => {
         <div className="chatMenu">
           <div className="chatMenuWrapper">
             <input placeholder="Search for friends" className="chatMenuInput" />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((c) => (
+              <Conversation key={c._id} conversation={c} currentUser={user} />
+            ))}
           </div>
         </div>
         <div className="chatBox">
